6db1344c66a51d89bcfb3c972df412a7312288b3
Benjamin Nguyen <ikarostdx@gmail.com>
2021-07-29 17:42:41 -0700

    Add login and Register interface

---
+++ javascript/switchLogin.js
@@ -0,0 +1,14 @@
+let x = document.getElementById("login");
+let y = document.getElementById("register");
+let z = document.getElementById("button");
+
+function register(){
+    x.style.left="-380px"
+    y.style.left="-0px"
+    z.style.left = "120px"
+}
+function login(){
+    x.style.left="0px"
+    y.style.left="380px"
+    z.style.left = "0px"
+}
