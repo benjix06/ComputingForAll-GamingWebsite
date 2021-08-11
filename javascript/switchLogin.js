101367690d56d7076215602ae9585c7dfc3c3ca6
Connor-CM <connordechangebringer@computingforall.org>
2021-08-11 14:15:12 -0700

    Updating branch

--- javascript/switchLogin.js
+++ javascript/switchLogin.js
@@ -12,3 +12,45 @@ function login(){
     registerBox.style.left="380px"
     topColorButton.style.left = "0px"
 } 
+
+function getInfo(){
+
+    // Create the account
+    let userName = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let confirmPassword = document.getElementById("confirmpassword").value;
+    let email = document.getElementById("email").value;
+
+
+    if(password === confirmPassword){
+        console.log("Password matched!")
+    }
+}
+
+function check(){
+    
+    let dic = {
+
+        userName: "test",
+        password: "password"
+
+    }
+
+    var passWord = document.getElementById("passWord").value;
+    var userName = document.getElementById("UserName").value;
+
+    if(passWord === dic.password && userName === dic.userName){
+        console.log("Logged in");
+        window.location.href = "./Logged-in-html/index1.html";
+    }
+    else if(passWord === "handsomeNoDoubt" && userName === "prettyR"){
+        console.log("Logged in");
+        window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    }
+    else{
+        console.log("failed");
+        alert("Only the chosen one can log in!\n But maybe try:\n usernam: prettyR\n password: handsomeNoDoubt");
+    }
+
+
+}
